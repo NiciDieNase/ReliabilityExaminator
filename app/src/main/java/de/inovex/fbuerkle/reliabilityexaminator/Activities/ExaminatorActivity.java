95727f95da5cc1801d00ffce1cb14477cce43a05
Felix Bürkle <f.buerkle@inovex.de>
2016-11-22 18:29:56 +0100

    Let FAB do something usefull

--- app/src/main/java/de/inovex/fbuerkle/reliabilityexaminator/Activities/ExaminatorActivity.java
+++ app/src/main/java/de/inovex/fbuerkle/reliabilityexaminator/Activities/ExaminatorActivity.java
@@ -2,7 +2,6 @@ package de.inovex.fbuerkle.reliabilityexaminator.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -43,8 +42,10 @@ public class ExaminatorActivity extends AppCompatActivity implements SelectADFDi
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+//				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//						.setAction("Action", null).show();
+				mADFaction = ADFaction.load;
+				new SelectADFDialog().show(getFragmentManager(),"selectADF");
 			}
 		});
 
